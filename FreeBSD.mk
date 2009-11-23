107e5814538b4a5b85107c7ac6078ff870073aa6
Douglas William Thrift <douglas@douglasthrift.net>
2009-11-23 03:25:24 +0000

    Fix build on FreeBSD.

--- FreeBSD.mk
+++ FreeBSD.mk
@@ -1,3 +1,3 @@
 export PATH := /usr/local/bin:/usr/local/GNUstep/System/Tools:$(PATH)
-gcc := g++44
+gcc := g++42
 include PkgConfig.mk
--- PkgConfig.mk
+++ PkgConfig.mk
@@ -1,6 +1,6 @@
 ifneq ($(shell pkg-config webkit-1.0 --modversion 2>/dev/null),)
 flags += $(shell pkg-config --cflags webkit-1.0)
-library += -lwebkit-1.0
+library += $(shell pkg-config --libs webkit-1.0)
 include Execute.mk
 else
 ifneq ($(shell pkg-config WebKitGtk --modversion 2>/dev/null),)
