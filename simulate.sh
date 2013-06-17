c15969fd0a6280c60d2f35876b5343004fed2b11
Jay Freeman (saurik) <saurik@saurik.com>
2013-06-17 03:57:32 -0700

    Upgrade copyright notice (2013, mk, sh, GPLv3).

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Cycript - Optimizing JavaScript Compiler/Runtime
+# Copyright (C) 2009-2013  Jay Freeman (saurik)
+
+# GNU General Public License, Version 3 {{{
+#
+# Cycript is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Cycript is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cycript.  If not, see <http://www.gnu.org/licenses/>.
+# }}}
+
 export DYLD_ROOT_PATH=$(xcode-select --print-path)/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator6.1.sdk
 export DYLD_FALLBACK_LIBRARY_PATH=/usr/lib
 export DYLD_FALLBACK_FRAMEWORK_PATH=/System/Library/Frameworks
