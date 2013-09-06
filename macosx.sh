81d7bcec953ab7e1a918fe1d90d1a754043a3840
Jay Freeman (saurik) <saurik@saurik.com>
2013-09-06 03:45:23 -0700

    Bypass the autotools build environment on Mac OS X.

--- macosx.sh
+++ macosx.sh
@@ -6,7 +6,15 @@ version=6.2
 readline=readline-${version}
 
 if [[ ! -d "${readline}" ]]; then
-    ./readline.sh "${version}"
-fi
+    ./readline.sh "${version}"; fi
+
+if ! which aclocal; then
+    touch aclocal.m4; fi
+if ! which autoconf; then
+    touch configure.ac; fi
+if ! which automake; then
+    touch Makefile.in; fi
+if ! which autoheader; then
+    touch config.h.in; fi
 
 exec "${0%%/*}/configure" CPPFLAGS="-I${readline}" LDFLAGS="-L${readline}" "$@"
