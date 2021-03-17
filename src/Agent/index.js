f6036b758fd1c1c65484cce606628beba68a0def
lateralusd <xdaemonx@protonmail.ch>
2021-03-17 13:35:27 +0100

    Remove choose part from agent

--- src/Agent/index.js
+++ src/Agent/index.js
@@ -159,16 +159,3 @@ function dlopen(library, mode) {
   const path = Memory.allocUtf8String(library);
   return _dlopen(path, mode);
 }
-
-global.choose = function(className) {
-  const objects = []
-  var classToFind = eval('ObjC.classes.' + className);
-  ObjC.choose(classToFind, {
-    onMatch: function(a) {
-      objects.push(a.toString());
-    },
-    onComplete: function() {
-    }
-  });
-  return objects;;
-};
